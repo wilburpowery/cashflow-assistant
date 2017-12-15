4c320199d452f5126364c83ed5a903ed31183c03
Wilbur Powery <wilburpowery@gmail.com>
2017-12-15 17:41:15 -0600

    wip

--- database/migrations/2017_12_15_105123_create_desks_table.php
+++ database/migrations/2017_12_15_105123_create_desks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDeskClosesTable extends Migration
+class CreateDesksTable extends Migration
 {
     /**
      * Run the migrations.
